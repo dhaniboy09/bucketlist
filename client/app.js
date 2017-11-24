3d27a01b432126f172d1ca180457c7e7f41b02db
Dhani <Dhani@Dhani.local>
2017-11-24 12:59:23 +0100

    Restructure file structure - Begin docker setup for client side

---
+++ client/app.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
